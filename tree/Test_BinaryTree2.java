b1df8fa89436838bdeb1c176b564a65be223f9b1
Sameer <salrehaili@gmail.com>
2021-12-08 09:00:12 +0300

    TreeV2

--- tree/Test_BinaryTree2.java
+++ tree/Test_BinaryTree2.java
@@ -57,7 +57,7 @@ class BinaryTree{
 		// System.out.println();
 		// for(int i=0;i<margin*k;i++)
 		// 	System.out.print(" ");
-		System.out.printf("\n%"+margin*k+"s", " ");
+		System.out.printf("%"+margin*k+"s", " ");
 
 		if(f=='r')
 			System.out.printf("┌─%d──│\n", root.getValue());
@@ -69,7 +69,33 @@ class BinaryTree{
 		display(root.left, margin +1, 'l');
 
 	}
-	
+
+	public void insert(Node curr, Object source, Object destination){
+		if(curr == null && this.root == null)
+		{
+			this.root = new Node(source);
+			this.root.left = new Node(destination);
+		}
+		else if(curr.getValue() == source)
+		{
+			if(curr.left == null)
+				curr.left = new Node(destination);
+			else
+				curr.right = new Node(destination);
+		}
+		
+		if(curr !=null){
+			if (curr.left != null)
+				insert(curr.left, source, destination);
+			if (curr.right != null)
+				insert(curr.right, source, destination);
+		}
+		
+
+	}
+
+
+
 	public void PLR(Node root){
 		if(root==null)
 			return;
@@ -97,39 +123,54 @@ public void LRP(Node root){
 	
 
 }
-public class Test_BinaryTree{
+public class Test_BinaryTree2{
 	public static void main(String[] args){
 		BinaryTree tree = new BinaryTree();
-		tree.root = new Node(1);
-		tree.root.left = new Node(2);
-		tree.root.right = new Node(3);
-		tree.root.left.left = new Node(4);
+		// tree.root = new Node(1);
+		// tree.root.left = new Node(2);
+		// tree.root.right = new Node(3);
+		// tree.root.left.left = new Node(4);
 
-		tree.root.left.left.left = new Node(8);
-		tree.root.left.left.right = new Node(9);
+		// tree.root.left.left.left = new Node(8);
+		// tree.root.left.left.right = new Node(9);
 
-		tree.root.left.right = new Node(5);
+		// tree.root.left.right = new Node(5);
 
-		tree.root.left.right.left = new Node(10);
+		// tree.root.left.right.left = new Node(10);
 
-		tree.root.left.right.right = new Node(11);
+		// tree.root.left.right.right = new Node(11);
 
-		tree.root.right.left = new Node(6);
-		tree.root.right.left.right = new Node(13);
-		tree.root.right.right = new Node(7);
-		tree.root.right.right.left = new Node(14);
+		// tree.root.right.left = new Node(6);
+		// tree.root.right.left.right = new Node(13);
+		// tree.root.right.right = new Node(7);
+		// tree.root.right.right.left = new Node(14);
 
 
+
+		tree.insert(tree.root, 1,2);
+		tree.insert(tree.root, 1,3);
+		tree.insert(tree.root, 2,4);
+		tree.insert(tree.root, 2,5);
+		tree.insert(tree.root, 4,8);
+		tree.insert(tree.root, 4, 9);
+		tree.insert(tree.root, 5, 10);
+		tree.insert(tree.root,5, 11);
+		tree.insert(tree.root, 3, 6);
+		tree.insert(tree.root, 3,7);
+		tree.insert(tree.root, 6,13);
+		tree.insert(tree.root, 7,14);
+
+		
 		//System.out.println(tree.root.getValue());
 		tree.display(tree.root, 1, 'c');
 
-		tree.PLR(tree.root);
+		tree.PLR(tree.root);	// pr-eorder
 		System.out.println();
 
-		tree.LPR(tree.root);
+		tree.LPR(tree.root);	// in-order
 		System.out.println();
 
-		tree.LRP(tree.root);
+		tree.LRP(tree.root); //post-order
 		System.out.println();
 
 	}
